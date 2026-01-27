57e8bf38fc4640e9742a69add6ee0f3c3f5a081d
phead <65379136@qq.com>
2026-01-27 13:05:10 +0800

    clean up dist

--- cdk/dist/lib/vpc-construct.d.ts
+++
@@ -1,6 +0,0 @@
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { Construct } from 'constructs';
-export declare class VpcConstruct extends Construct {
-    readonly vpc: ec2.Vpc;
-    constructor(scope: Construct, id: string);
-}
