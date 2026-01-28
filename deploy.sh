f65ea68e186c6022ae712e1953668ee338102fd6
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2026-01-28 17:47:50 +0530

    deploy code update

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@
 export CDK_DEFAULT_ACCOUNT=${AWS_ACCOUNT_ID}
 export CDK_DEFAULT_REGION=${AWS_REGION}
 cd submission-watcher-lambda
-npm install
+npm ci --only=production --no-audit --no-fund
 cd ..
+export SIMPLE_BUNDLING=true
+export CI=true
 cd cdk
 npm install
 npm run build
 npm run deploy
+
